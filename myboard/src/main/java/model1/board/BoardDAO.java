f59bd6cef02583e8dbe154794d555d71af05c6e4
woodawon <woodawon0602@gmail.com>
2023-11-09 15:54:36 +0900

    board list

---
+++ myboard/src/main/java/model1/board/BoardDAO.java
@@ -0,0 +1,66 @@
+package model1.board;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import common.DBConnPool;
+
+public class BoardDAO extends DBConnPool {
+	public BoardDAO() {
+		super();
+	}
+
+	public int selectCount(Map<String, Object> map) {
+		int totalCount = 0;
+		String query = "select count(*) from board";
+		if (map.get("searchWord") != null) {
+			query += " where " + map.get("searchField") + " like '%" + map.get("searchWord") + "%'";
+		}
+
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+			rs.next();
+			totalCount = rs.getInt(1);
+		} catch (Exception e) {
+			System.out.println("게시물 수를 구하는 중 예외 발생");
+			e.printStackTrace();
+		}
+
+		return totalCount;
+	}
+
+	public List<BoardDTO> selectList(Map<String, Object> map) {
+		List<BoardDTO> bbs = new Vector<>();
+
+		String query = "select * from board";
+		if (map.get("searchWord") != null) {
+			query += " where " + map.get("searchField") + " like '%" + map.get("searchWord") + "%'";
+		}
+		
+		query += " order by num desc"; // num 을 내림차순 정렬 하기 
+		
+		try {
+			stmt = con.createStatement();
+			rs= stmt.executeQuery(query);
+			
+			while(rs.next()) {
+				BoardDTO dto = new BoardDTO();
+				dto.setNum(rs.getString("num"));
+				dto.setTitle(rs.getString("title"));
+				dto.setContent(rs.getString("content"));
+				dto.setPostdate(rs.getDate("postdate"));
+				dto.setId(rs.getString("id"));
+				dto.setVisitcount(rs.getString("visitcount"));
+				
+				bbs.add(dto);
+			}
+		} catch (Exception e) {
+			System.out.println("게시물 조회 중 예외 발생");
+			e.printStackTrace();
+		}
+
+		return bbs;
+	}
+}
