c388700ffa5ca468221ec574dc0f86c571c22abc
woodawon <woodawon0602@gmail.com>
2023-11-20 14:38:56 +0900

    mvclist

--- mvcboard/src/main/java/kr/seoul/mvcboard/MVCBoardDAO.java
+++ mvcboard/src/main/java/kr/seoul/mvcboard/MVCBoardDAO.java
@@ -11,39 +11,56 @@ public class MVCBoardDAO extends DBConnPool {
 		super();
 	}
 	
-	public int selectCount(Map<String, Object> map) {
+	private String getWhere(Map<String, Object> map, String query) { 
+		// 검색 기능
+		if (map.get("searchWord") != null) {
+			query += " where" + map.get("searchField") + " like '%" + map.get("searchWord") + "%'";
+		}
+		return query;
+	}
+
+	public int selectCount(Map<String, Object> map) { 
+		// 게시물 개수 조회
 		int totalcount = 0;
-		
+
 		String query = "select count(*) from mvcboard";
+		query = getWhere(map, query);
+
 		try {
 			stmt = con.createStatement();
 			rs = stmt.executeQuery(query);
 			rs.next();
 			totalcount = rs.getInt(1);
 		} catch (Exception e) {
-			System.out.println("카운트 중 예외 발생");
 			e.printStackTrace();
 		}
-		
+
 		return totalcount;
 	}
-	
+
 	public List<MVCBoardDTO> selectListPage(Map<String, Object> map) {
+		// 게시물 목록 조회
 		List<MVCBoardDTO> board = new Vector<>();
 		
-		String query = "select * from ("
-				+ " select Tb.*, rownum rNum from ("
-				+ " select * from mvcboard"
-				+ " order by idx desc) )"
-				+ " where rNum between ? and ?";
+		String query = "select * from mvcboard"
+				+ " where title like '%title%'";
+		
+		query = getWhere(map, query);
+		
+		query += " order by idx desc limit ?,?";
 		
 		try {
+			
 			psmt = con.prepareStatement(query);
-			psmt.setString(1, map.get("start").toString());
-			psmt.setString(2, map.get("end").toString());
+			psmt.setInt(1, Integer.parseInt(map.get("start").toString()));
+			psmt.setInt(2, Integer.parseInt(map.get("end").toString()));
 			rs = psmt.executeQuery();
+			
 			while(rs.next()) {
+				
 				MVCBoardDTO dto = new MVCBoardDTO();
+				// DB에 있는 게시물의 컬럼 값들을 받아와서 DTO에 저장한다.
+				
 				dto.setIdx(rs.getString(1));
 				dto.setName(rs.getString(2));
 				dto.setTitle(rs.getString(3));
@@ -51,16 +68,19 @@ public class MVCBoardDAO extends DBConnPool {
 				dto.setPostdate(rs.getDate(5));
 				dto.setOfile(rs.getString(6));
 				dto.setSfile(rs.getString(7));
-				dto.setDowncount(rs.getInt(8));
+				dto.setDowncount(rs.getInt(8)); // int
 				dto.setPass(rs.getString(9));
-				dto.setVisitcount(rs.getInt(10));
+				dto.setVisitcount(rs.getInt(10)); // int 
+				
 				board.add(dto);
 			}
+			
 		} catch (Exception e) {
-			System.out.println("게시물 조회 중 예외 발생");
 			e.printStackTrace();
 		}
 		
 		return board;
 	}
+
+	
 }
